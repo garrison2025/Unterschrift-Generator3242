17adfad27b6e224d2d3c25b796d1600c4cc823e2
garrison2025 <zhongsgeng@gmail.com>
2025-08-15 11:35:10 +0200

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,5 +1,8 @@
 import type { MetadataRoute } from "next"
 
+// 👇 在这里添加这行代码，以声明这是一个静态路由
+export const dynamic = 'force-static'
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
