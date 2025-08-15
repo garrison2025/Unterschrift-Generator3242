eca41784434190d9f5cb3c1d03dcbe0184b79cf4
garrison2025 <zhongsgeng@gmail.com>
2025-08-15 11:25:24 +0200

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: ["/private/", "/admin/"],
     },
-    sitemap: "https://unterschrift-generator.de/sitemap.xml",
+    sitemap: "https://unterschriftgenerator.pro/sitemap.xml",
   }
 }
